11859f4c78f18136e49d890154d8b14990252554
Yujin <yujin@Yujinui-MacBookAir.local>
2020-07-23 19:54:30 +0900

    login

--- reference/firebaseWebLogin/login.js
+++ reference/firebaseWebLogin/login.js
@@ -1,27 +1,26 @@
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
+(function() {
 
-    document.getElementById("user_div").style.display = "block";
-    document.getElementById("login_div").style.display = "none";
+  const config = {
+      apiKey: "AIzaSyCqHKmX1adJ9qCPMnSWyP1p5JRzEruoe1E",
+      authDomain: "fittogether-d628a.firebaseapp.com",
+      databaseURL: "https://fittogether-d628a.firebaseio.com",
+      projectId: "fittogether-d628a",
+      storageBucket: "fittogether-d628a.appspot.com",
+      messagingSenderId: "169218684132",
+      appId: "1:169218684132:web:3134f8df18470da201a8f1",
+      measurementId: "G-2MK17W9C5C"
+    };
+    firebase.initializeApp(config);
+};
 
-    var user = firebase.auth().currentUser;
-
-    if(user != null){
-
-      var email_id = user.email;
-      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
-
-    }
-
-  } else {
-    // No user is signed in.
-
-    document.getElementById("user_div").style.display = "none";
-    document.getElementById("login_div").style.display = "block";
-
+firebase.auth().onAuthStateChanged(firebaseUser=> {
+  if(firebaseUser) {
+    console.log(firebaseUser);
   }
-});
+  else {
+    console.log('not logged in');
+  }
+}
 
 function login(){
 
@@ -35,81 +34,25 @@ function login(){
 
     window.alert("Error : " + errorMessage);
 
-    // ...
-  });
+  };
 
 }
 
 function logout(){
   firebase.auth().signOut();
-}
+});
 
+/* logout 참고
+firebase.auth().onAuthStateChanged(firebaseUser => {
+  if(firebaseUser) {
+    console.log(firebaseUser);
+    btnLogou.classList.remove('hide'); //logout button 숨김해제
+  }
+  else {
+    console.log('not logged in');
+    btnLogou.classList.add('hide');
+  }
+  }
+});
 
-/* 여기서부터는 회원가입 */
-
-// Your web app's Firebase configuration
-var firebaseConfig = {
-  //firebase config stuff
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-    
-    const auth = firebase.auth();
-    
-    
-    function signUp(){
-      
-      var email = document.getElementById("email");
-      var password = document.getElementById("password");
-      
-      const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
-      promise.catch(e => alert(e.message));
-      
-      alert("Signed Up");
-    }
-    
-    
-    
-    function signIn(){
-      
-      var email = document.getElementById("email");
-      var password = document.getElementById("password");
-      
-      const promise = auth.signInWithEmailAndPassword(email.value, password.value);
-      promise.catch(e => alert(e.message));
-      
-      
-      
-      
-    }
-    
-    
-    function signOut(){
-      
-      auth.signOut();
-      alert("Signed Out");
-      
-    }
-    
-    
-    
-    auth.onAuthStateChanged(function(user){
-      
-      if(user){
-        
-        var email = user.email;
-        alert("Active User " + email);
-        
-        //Take user to a different or home page
-  
-        //is signed in
-        
-      }else{
-        
-        alert("No Active User");
-        //no user is signed in
-      }
-      
-      
-      
-    });  
+*/
