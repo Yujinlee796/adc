626e8692a2ea41e25af5c0a40bb6d4077d3c8bea
Chaeyoung <l_chyoung@snu.ac.kr>
2020-08-09 20:17:43 +0900

    roomFunctionAdded

--- ongoing/main/js/mainRoom.js
+++ ongoing/main/js/mainRoom.js
@@ -34,21 +34,8 @@ function openPunishmentPopup() {
   var punishmentPopupOption = "width=570, height=250, resizable = no, scrollbars = no"
   window.open(punishmentPopupUrl, '', punishmentPopupOption, '');
 }
-
-//버튼
-function successImg() {
-  document.getElementById("profileImg").src = "assets/img/charCatSuccess.png";
-}
-function failImg() {
-  document.getElementById("profileImg").src = "assets/img/charCatFail.png"
-}
-function reset() {
-  document.getElementById("profileImg").src = "assets/img/charCat.png"
-}
-                      
+                  
 //성공 버튼 누르면 점수가 올라가는 함수
-var todayScore = 0;
-
 function plusOne() {
   if (todayScore===0) {
     todayScore = todayScore + 1;
@@ -56,21 +43,20 @@ function plusOne() {
   else if (todayScore>0) {
     alert("오늘은 이미 성공 버튼을 누르셨습니다.");
   }
-  else if (todayScore===null) {
+  else if (todayScore===100) {
     alert("오늘은 이미 포기 버튼을 누르셨습니다.");
   };
 };
-
-//포기 버튼 누르면 값이 null로 바뀌는 함수
+//포기 버튼 누르면 값이 100으로 바뀌는 함수
 
 function giveUp() {
   if (todayScore===0) {
-    todayScore = null;
+    todayScore = 100;
   }
   else if (todayScore===1) {
     alert("오늘은 이미 성공 버튼을 누르셨습니다.");
   }
-  else if (todayScore===null) {
+  else if (todayScore===100) {
     alert("오늘은 이미 포기 버튼을 누르셨습니다.");
   };
 };
@@ -81,4 +67,39 @@ function resetScore() {
   todayScore = 0;
 };
 
-console.log(todayScore);
+//성공 버튼 누르면 고양이 그림 바꾸는 함수
+function successImg() {
+  if (todayScore === 0) {
+    document.getElementById("profileImg").src = "assets/img/charCatSuccess.png";
+  }
+  else {
+
+  };
+};
+
+//포기 버튼 누르면 고양이 그림 바꾸는 함수
+function failImg() {
+  if (todayScore === 0) {
+    document.getElementById("profileImg").src = "assets/img/charCatFail.png";
+  }
+  else {
+
+  };
+};
+
+//리셋 버튼 누르면 고양이 그림 바꾸는 함수
+function reset() {
+  document.getElementById("profileImg").src = "assets/img/charCat.png";
+};
+
+//새벽 5시가 지나면 todayScore값을 0으로 바꾸는 함수
+
+if (currentHours === 5 && currentMinutes === 0) {
+  todayScore = 0;
+  if (todayScore === 1) {
+    totalScore = totalScore + 1;
+  }
+  else {
+
+  };
+};
