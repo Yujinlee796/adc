5abaa4d698294412cef14053c24d8aea398c6922
Yujinlee796 <yujin@Yujinui-MacBookAir.local>
2020-09-06 17:02:39 +0900

    유진

--- ongoing/mobile/main/js/mainRoomCertify.js
+++ ongoing/mobile/main/js/mainRoomCertify.js
@@ -13,7 +13,7 @@ var firebaseConfig = {
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
   var database = firebase.database();
-  
+
   firebase.auth.Auth.Persistence.LOCAL;
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -86,7 +86,7 @@ function usersroomUpdate() {
         }
         });
 
-        firebase.database().ref('Rooms/' + roomName + '/Users/'+ currentUserID + '/').set({
+        firebase.database().ref('Rooms/' + roomName + '/Users/'+ currentUserID).set({
             fitcnt : fitcnt,
         },
         function(error) {
@@ -110,18 +110,18 @@ function getTimeStamp() {
       leadingZeros(d.getFullYear(), 4) + '-' +
       leadingZeros(d.getMonth() + 1, 2) + '-' +
       leadingZeros(d.getDate(), 2) + ' ' +
-    
+
       leadingZeros(d.getHours(), 2) + ':' +
       leadingZeros(d.getMinutes(), 2) + ':' +
       leadingZeros(d.getSeconds(), 2);
-    
+
     return s;
     }
-    
+
 function leadingZeros(n, digits) {
     var zero = '';
     n = n.toString();
-    
+
     if (n.length < digits) {
       for (i = 0; i < digits - n.length; i++)
         zero += '0';
